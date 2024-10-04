705cbdc487a951b693b6871d0fb6b1736ada60e5
Jonta Sancar <jontasancar@gmail.com>
2024-10-04 13:42:02 -0300

    correção para funcionamento do hammer

--- rotate.js
+++ rotate.js
@@ -1,13 +1,17 @@
-let currentRotation = 0;
-
-// Detecta o gesto de rotação (twist com dois dedos)
-hammer.get('rotate').set({ enable: true });
-
-// Aplica a rotação ao mapa com base no gesto
-hammer.on('rotate', function(event) {
-  currentRotation += event.rotation;  // Acumula o valor da rotação
-  mapContainer.style.transform = `rotate(${currentRotation}deg)`;
-  // rotateMap(currentRotation)
-});
-
-hammer.on('rotateend', ()=>{}); // Quando o gesto de rotação terminar, aplica o ângulo final ao mapa
+function enableRotateByHammer(){
+  const mapContainer = document.getElementById('map');
+  const hammer = new Hammer(mapContainer);
+      
+  let currentRotation = 0;
+  
+  // Detecta o gesto de rotação (twist com dois dedos)
+  hammer.get('rotate').set({ enable: true });
+  
+  // Aplica a rotação ao mapa com base no gesto
+  hammer.on('rotate', function(event) {
+    currentRotation += event.rotation;  // Acumula o valor da rotação
+    rotateMap(currentRotation)
+  });
+  
+  hammer.on('rotateend', ()=>{}); // Quando o gesto de rotação terminar, aplica o ângulo final ao mapa
+}
