2e7e9aba9c79fbe465b2c4523b03822ecf42d161
Jonta Sancar <jontasancar@gmail.com>
2024-10-04 13:29:42 -0300

    usando rotação padrão

--- rotate.js
+++ rotate.js
@@ -6,8 +6,8 @@ hammer.get('rotate').set({ enable: true });
 // Aplica a rotação ao mapa com base no gesto
 hammer.on('rotate', function(event) {
   currentRotation += event.rotation;  // Acumula o valor da rotação
-  // mapContainer.style.transform = `rotate(${currentRotation}deg)`;
-  rotateMap(currentRotation)
+  mapContainer.style.transform = `rotate(${currentRotation}deg)`;
+  // rotateMap(currentRotation)
 });
 
 hammer.on('rotateend', ()=>{}); // Quando o gesto de rotação terminar, aplica o ângulo final ao mapa
