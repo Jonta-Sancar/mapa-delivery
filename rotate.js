6d1526f183ce11a7b45e69d2ad11f7c97477426f
Jonta Sancar <jontasancar@gmail.com>
2024-10-08 16:08:18 -0300

    correção de funcionamento do hammer

--- rotate.js
+++ rotate.js
@@ -1,17 +1,35 @@
 function enableRotateByHammer(){
-  const mapContainer = document.getElementById('map');
+  const circle   = document.querySelector('.circle');
+
+  const mapContainer = document.querySelector('#map');
   const hammer = new Hammer(mapContainer);
       
-  let currentRotation = 0;
+  let current_rotation = 0;
+  let element_rotation = 0;
+  let initial_rotation = 0;
+  let final_rotation = 0;
   
   // Detecta o gesto de rotação (twist com dois dedos)
   hammer.get('rotate').set({ enable: true });
   
+  // Pega a rotação atual do círculo e inicia o ajuste de rotação
+  hammer.on('rotatestart', function(event) {
+    element_rotation = Number(circle.dataset.rotation);
+    initial_rotation = event.rotation; // Armazena a rotação inicial do gesto
+  });
+
   // Aplica a rotação ao mapa com base no gesto
   hammer.on('rotate', function(event) {
-    currentRotation += event.rotation;  // Acumula o valor da rotação
-    rotateMap(currentRotation)
+    current_rotation   = Number((event.rotation - initial_rotation).toFixed(2));  // pega a parcela de rotação
+
+    final_rotation = current_rotation + element_rotation;
+    
+    circle.style.transform = `rotate(${final_rotation}deg)`;
+    document.querySelector('.describe').innerHTML = final_rotation;
+    rotateMap(final_rotation);
   });
   
-  hammer.on('rotateend', ()=>{}); // Quando o gesto de rotação terminar, aplica o ângulo final ao mapa
+  hammer.on('rotateend', ()=>{
+    circle.dataset.rotation = final_rotation;
+  }); // Quando o gesto de rotação terminar, aplica o ângulo final ao mapa
 }
